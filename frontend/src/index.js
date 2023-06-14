3908b89d6a2f9d67c6f6ce0364cffa26318c9e11
Sakshi Mulekar <sakshimulekar2017@gmail.com>
2023-06-14 12:28:10 +0530

    store added by sak

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
 import {ChakraProvider} from "@chakra-ui/react"
+import { store } from './redux/Store';
+import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
   <BrowserRouter>
     <ChakraProvider>
       <App />
     </ChakraProvider>
   </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
